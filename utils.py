ffb08b2f5809c360f0ba47e4b9e065d1c741b0af
Domiciano <guilhermedomicianosilva@gmail.com>
2025-04-04 14:24:24 -0300

    fix

--- utils.py
+++
@@ -1,7 +0,0 @@
-def get_user_input(prompt):
-    """Captura entrada do usuário de forma segura."""
-    try:
-        return input(prompt)
-    except KeyboardInterrupt:
-        print("\nJogo encerrado.")
-        exit()
